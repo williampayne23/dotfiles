c331f2fd4448f31a62dd13898c867ca6e660c8c6
Will Payne <will.payne@dsit.gov.uk>
2024-03-15 15:02:53 +0000

    Antidote + Starship

--- zsh/.config/zsh/zshinit.zsh
+++ zsh/.config/zsh/zshinit.zsh
@@ -1,5 +1,6 @@
 # Antigen
-source ~/.config/zsh/antigenrc.zsh
+source ~/.config/zsh/antidote/antidote.zsh
+antidote load ~/.config/zsh/zsh_plugins.txt
 
 # source all extra files
 for file in ~/.config/zsh/extra/*.zsh; do
@@ -11,3 +12,6 @@ eval "$(zoxide init zsh)"
 
 #atuin setup
 eval "$(atuin init zsh)"
+
+#starship setup
+eval "$(starship init zsh)"
