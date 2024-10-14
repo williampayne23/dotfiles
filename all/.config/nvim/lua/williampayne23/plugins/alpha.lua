d5c8bacba82e5efa5ca9f02d3954e9406560efad
Will.Payne <Will.Payne@dsit.gov.uk>
2024-10-14 09:52:38 +0000

    Remove comment

--- all/.config/nvim/lua/williampayne23/plugins/alpha.lua
+++ all/.config/nvim/lua/williampayne23/plugins/alpha.lua
@@ -5,14 +5,6 @@ return {
         dependencies = { 'nvim-tree/nvim-web-devicons' },
         config = function()
             require("williampayne23.alphatheme").setup()
-            -- vim.api.nvim_create_autocmd("User", {
-            --     pattern = "AlphaReady",
-            --     callback = require("williampayne23.alphatheme").on_open,
-            -- })
-            -- vim.api.nvim_create_autocmd("User", {
-            --     pattern = "AlphaClosed",
-            --     callback = require("williampayne23.alphatheme").on_close,
-            -- })
             vim.api.nvim_create_autocmd("User", {
                 pattern = "LazyVimStarted",
                 once = true,
