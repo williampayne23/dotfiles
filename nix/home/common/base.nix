084bb15519222ec2f251f642b7ee58d83faf1a02
Will.Payne <Will.Payne@dsit.gov.uk>
2025-01-16 11:44:49 +0000

    Working poetry

--- nix/home/common/base.nix
+++ nix/home/common/base.nix
@@ -2,10 +2,23 @@
   config,
   pkgs,
   ...
-}: {
+}: let 
+    customPoetry = pkgs.writeShellScriptBin "poetry" ''
+            # Python packages come pre built and sometimes assume there are c libraries available on the system
+            # Usually because they are but the nix way is to isolate packages by default so we need to provide
+            # pointers to the right LD_LIBRARY_PATH to get it to work
+            # (Previously I did this for my whole environment but that reasonably breaks things so don't do that)
+            # links 
+            # https://www.reddit.com/r/NixOS/comments/1dexghb/im_just_about_done_with_nixos_how_do_i_get_a/
+            # https://discourse.nixos.org/t/what-package-provides-libstdc-so-6/18707/3
+            export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib/
+            export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [ pkgs.zlib ]}:$LD_LIBRARY_PATH"
+            exec ${pkgs.poetry}/bin/poetry $@
+            '';
+in {
   home.packages = [
+    customPoetry
     pkgs.cargo
-    pkgs.poetry
     pkgs.neovim
     pkgs.gh
     pkgs.tmux
