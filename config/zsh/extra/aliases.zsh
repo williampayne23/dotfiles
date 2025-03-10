36ca6492490a035ba5baed3a355c595bd47454ef
Will.Payne <Will.Payne@dsit.gov.uk>
2025-03-10 14:03:41 +0000

    Switch alias

--- config/zsh/extra/aliases.zsh
+++ config/zsh/extra/aliases.zsh
@@ -8,3 +8,45 @@ alias cd!=\\cd
 nr() {
     nix run nixpkgs#"$1" -- "${@:2}"
 }
+
+_switch_if_exists() {
+    # $1 should be branch name
+    # If branch exists (either locally or remote) git switch to it
+    # If branch does not exist, return an error
+    branch_name=$1
+    if git show-ref --verify --quiet refs/heads/$branch_name; then
+        git switch $branch_name
+    elif git show-ref --verify --quiet refs/remotes/origin/$branch_name; then
+        git switch $branch_name
+    else
+        return 1
+    fi
+}
+
+s () {
+        branch_name=$1
+        if [ "$branch_name" = "-h" ]
+        then
+                echo "Usage: s [branch_name]"
+                echo "Switch to branch_name or will/branch_name if it exists (prioritise branch_name), otherwise create a new branch called will/branch_name"
+                return
+        fi
+        if [ -z "$branch_name" ]
+        then
+                branch_name=main
+        fi
+        if _switch_if_exists $branch_name
+        then
+                return
+        fi
+        if [[ $branch_name != will/* ]]
+        then
+                branch_name="will/$branch_name"
+        fi
+        if _switch_if_exists $branch_name
+        then
+                return
+        fi
+        git switch -c $branch_name
+}
+
