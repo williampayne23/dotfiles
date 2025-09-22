e420ebc43397344eb818ada4bb8dec273d8cf72e
Will.Payne <Will.Payne@dsit.gov.uk>
2025-09-22 08:49:42 +0000

    Drop nvim unity sync it's not doing anything

--- config/nvim/lua/williampayne23/plugins/specialised/unity.lua
+++ config/nvim/lua/williampayne23/plugins/specialised/unity.lua
@@ -1,7 +1,9 @@
-return {
-    "apyra/nvim-unity-sync",
-    lazy = false,
-    config = function()
-        require("unity.plugin").setup()
-    end,
-}
+return {}
+-- This doesn't work as it hooks into nvim tree events and I use snacks tree :(
+-- return {
+--     "apyra/nvim-unity-sync",
+--     lazy = false,
+--     config = function()
+--         require("unity.plugin").setup()
+--     end,
+-- }
