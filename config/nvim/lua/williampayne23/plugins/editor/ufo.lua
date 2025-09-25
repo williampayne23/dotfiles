5f9854102d36bc1b92370c2511a6d94ff42f34ed
Will.Payne <Will.Payne@dsit.gov.uk>
2025-09-25 12:47:09 +0000

    Fix fold issue

--- config/nvim/lua/williampayne23/plugins/editor/ufo.lua
+++ config/nvim/lua/williampayne23/plugins/editor/ufo.lua
@@ -1,65 +1,65 @@
 -- Folds
 local handler = function(virtText, lnum, endLnum, width, truncate)
-    local newVirtText = {}
-    local suffix = (" 󰁂 %d "):format(endLnum - lnum)
-    local sufWidth = vim.fn.strdisplaywidth(suffix)
-    local targetWidth = width - sufWidth
-    local curWidth = 0
-    for _, chunk in ipairs(virtText) do
-        local chunkText = chunk[1]
-        local chunkWidth = vim.fn.strdisplaywidth(chunkText)
-        if targetWidth > curWidth + chunkWidth then
-            table.insert(newVirtText, chunk)
-        else
-            chunkText = truncate(chunkText, targetWidth - curWidth)
-            local hlGroup = chunk[2]
-            table.insert(newVirtText, { chunkText, hlGroup })
-            chunkWidth = vim.fn.strdisplaywidth(chunkText)
-            -- str width returned from truncate() may less than 2nd argument, need padding
-            if curWidth + chunkWidth < targetWidth then
-                suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
-            end
-            break
-        end
-        curWidth = curWidth + chunkWidth
-    end
-    table.insert(newVirtText, { suffix, "MoreMsg" })
-    return newVirtText
+	local newVirtText = {}
+	local suffix = (" 󰁂 %d "):format(endLnum - lnum)
+	local sufWidth = vim.fn.strdisplaywidth(suffix)
+	local targetWidth = width - sufWidth
+	local curWidth = 0
+	for _, chunk in ipairs(virtText) do
+		local chunkText = chunk[1]
+		local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+		if targetWidth > curWidth + chunkWidth then
+			table.insert(newVirtText, chunk)
+		else
+			chunkText = truncate(chunkText, targetWidth - curWidth)
+			local hlGroup = chunk[2]
+			table.insert(newVirtText, { chunkText, hlGroup })
+			chunkWidth = vim.fn.strdisplaywidth(chunkText)
+			-- str width returned from truncate() may less than 2nd argument, need padding
+			if curWidth + chunkWidth < targetWidth then
+				suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+			end
+			break
+		end
+		curWidth = curWidth + chunkWidth
+	end
+	table.insert(newVirtText, { suffix, "MoreMsg" })
+	return newVirtText
 end
 
 return {
-    -- Folds
-    "kevinhwang91/nvim-ufo",
-    dependencies = {
-        "kevinhwang91/promise-async",
-        "neovim/nvim-lspconfig",
-    },
-    event = "BufReadPost",
-    config = function()
-        vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-        vim.o.foldlevelstart = 99
-        local capabilities = vim.lsp.protocol.make_client_capabilities()
-        capabilities.textDocument.foldingRange = {
-            dynamicRegistration = false,
-            lineFoldingOnly = true,
-        }
-        local language_servers = vim.lsp.config.util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
-        for _, ls in ipairs(language_servers) do
-            require("lspconfig")[ls].setup({
-                capabilities = capabilities,
-                -- you can add other fields for setting up lsp server in this table
-            })
-        end
-        require("ufo").setup({
-            fold_virt_text_handler = handler,
-        })
-        vim.keymap.set("n", "K", function()
-            local winid = require("ufo").peekFoldedLinesUnderCursor()
-            if not winid then
-                -- choose one of coc.nvim and nvim lsp
-                -- vim.fn.CocActionAsync('definitionHover')     -- coc.nvim
-                vim.lsp.buf.hover({ border = "single", max_height = 25, max_width = 120 })
-            end
-        end)
-    end,
+	-- Folds
+	"kevinhwang91/nvim-ufo",
+	dependencies = {
+		"kevinhwang91/promise-async",
+		"neovim/nvim-lspconfig",
+	},
+	event = "BufReadPost",
+	config = function()
+		vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+		vim.o.foldlevelstart = 99
+		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		capabilities.textDocument.foldingRange = {
+			dynamicRegistration = false,
+			lineFoldingOnly = true,
+		}
+		local language_servers = vim.lsp.conifg.available_servers()
+		for _, ls in ipairs(language_servers) do
+			require("lspconfig")[ls].setup({
+				capabilities = capabilities,
+				-- you can add other fields for setting up lsp server in this table
+			})
+		end
+		require("ufo").setup({
+			fold_virt_text_handler = handler,
+		})
+		vim.keymap.set("n", "K", function()
+			local winid = require("ufo").peekFoldedLinesUnderCursor()
+			if not winid then
+				-- choose one of coc.nvim and nvim lsp
+				-- vim.fn.CocActionAsync('definitionHover')     -- coc.nvim
+				vim.lsp.buf.hover({ border = "single", max_height = 25, max_width = 120 })
+			end
+		end)
+	end,
 }
